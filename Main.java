cf91df46d76f46d0064c05b98e306bb7c82671e1
sidhuk-ai <kovisidhardha2004@gmail.com>
2024-05-07 02:24:08 +0530

    Basic CRUD application using Java JDBC

---
+++ Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ServerConnection controller = new ServerConnection();
+
+        System.out.println("Welcome to the CRUD Apllication!!\n1. Show Data\n2. Add Data\n3. Update Data\n4. Delete Data");
+
+        while (true) {
+            System.out.println("Choose the operations you have to perform(1-4)");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    controller.readTable();
+                    break;
+                case 2:
+                    System.out.print("Enter first name: ");
+                    String fname = sc.nextLine();
+
+                    System.out.print("\nEnter last name: ");
+                    String lname = sc.nextLine();
+
+                    System.out.print("\nEnter Age: ");
+                    int age = sc.nextInt();
+
+                    controller.createRecord(fname, lname, age);
+                    break;
+                case 3:
+                    System.out.println("Enter new First Name: ");
+                    String newName = sc.nextLine();
+                    System.out.println("Enter the Class Id: ");
+                    int Clid = sc.nextInt();
+                    controller.updateRecord(newName,Clid);
+                case 4:
+                    System.out.println("Enter First Name: ");
+                    String fName = sc.nextLine();
+                    System.out.println("Enter Last Name: ");
+                    String lName = sc.nextLine();
+                    System.out.println("Enter Age Name: ");
+                    int age_DB = sc.nextInt();
+
+                    controller.deleteRecord(fName, lName, age_DB);
+                default:
+                    break;
+            }
+        }
+
+
+
+    }
+}
---
+++ ServerConnection.java
@@ -0,0 +1,124 @@
+import java.sql.*;
+
+public class ServerConnection {
+
+    public void createDB(String dataBase){
+        try {
+            String url = "jdbc:mysql://localhost:3306/";
+            String userName = "root";
+            String password = "Sidhu@1234";
+            Connection conn = DriverManager.getConnection(url, userName, password);
+            Statement stm = conn.createStatement();
+            String query = "CREATE database " + dataBase;
+            stm.execute(query);
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createTable(String tableName){
+        try {
+            String url = "jdbc:mysql://localhost:3306/project";
+            String userName = "root";
+            String password = "Sidhu@1234";
+            Connection conn = DriverManager.getConnection(url, userName, password);
+            Statement stm = conn.createStatement();
+            String query = "CREATE TABLE " + tableName + " (ClassId int NOT NULL AUTO_INCREMENT, FirstName varchar(20) NOT NULL, LastName varchar(20), Age int, PRIMARY KEY(ClassId))";
+            stm.execute(query);
+
+            System.out.println("Table created successfully.");
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createRecord(String firstName,String lastName, int age){
+        try {
+            String url = "jdbc:mysql://localhost:3306/";
+            String db = "project";
+            String userName = "root";
+            String password = "Sidhu@1234";
+            Connection conn = DriverManager.getConnection(url+db, userName, password);
+            String query = "INSERT INTO Student (FirstName, LastName, Age) VALUES (?,?,?)";
+            PreparedStatement pstm = conn.prepareStatement(query);
+            pstm.setString(1, firstName);
+            pstm.setString(2, lastName);
+            pstm.setInt(3, age);
+            pstm.execute();
+            System.out.println("Recird added successfully.");
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readTable() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/";
+            String db = "project";
+            String userName = "root";
+            String password = "Sidhu@1234";
+            Connection conn = DriverManager.getConnection(url+db, userName, password);
+            Statement stm = conn.createStatement();
+            String query = "SELECT * FROM Student";
+            ResultSet rs = stm.executeQuery(query);
+            while(rs.next()){
+                System.out.println("Id -->"+rs.getInt(1));
+                System.out.println("First Name -->"+rs.getString(2));
+                System.out.println("Last Name -->"+rs.getString(3));
+                System.out.println("Age -->"+rs.getInt(4));
+            }
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+    // public void updateRecord(){
+        public void updateRecord(String newName, int classID){
+            try {
+                String url = "jdbc:mysql://localhost:3306/";
+                String db = "project";
+                String userName = "root";
+                String password = "Sidhu@1234";
+                Connection conn = DriverManager.getConnection(url+db, userName, password);
+                String query = "UPDATE Student SET FirstName=? WHERE ClassId=?";
+                PreparedStatement pstm = conn.prepareStatement(query);
+                pstm.setString(1, newName);
+                pstm.setInt(2, classID);
+                pstm.execute();
+                System.out.println("Record updated successfully.");
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteRecord(String firstName, String lastName, int age) {
+        String url = "jdbc:mysql://localhost:3306/project";
+        String username = "root";
+        String password = "Sidhu@1234";
+
+        String sql = "DELETE FROM Student WHERE FirstName=? AND LastName=? AND Age=?";
+
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, firstName);
+            pstmt.setString(2, lastName);
+            pstmt.setInt(3, age);
+
+            int rowsDeleted = pstmt.executeUpdate();
+
+            if (rowsDeleted > 0) {
+                System.out.println("Record deleted successfully");
+            } else {
+                System.out.println("No such record exists");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
